aaa80cb784733df640ba9c800b445fb810e40019
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-02-08 13:16:24 +0000

    add basic tests in core_app/tests.py for the core_app application

--- skillpilot/core_app/tests.py
+++ skillpilot/core_app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from core_app.views import homepage, internship, student
 
-# Create your tests here.
+class HttpResponseTest(TestCase):
+  def test_homepage_status_code(self):
+    response = self.client.get('/')
+    self.assertEqual(response.status_code, 200)
+
+  def test_pagenotfound_status_code(self):
+    response = self.client.get('/this-page-doesnt-exist')
+    self.assertEqual(response.status_code, 404)
+
+  # more tests here
+
+class URLsTest(TestCase):
+  def test_homepage_template_rendering(self):
+    response = self.client.get(reverse('homepage'))
+    self.assertTemplateUsed(response, "homepage.html")
+
+  # more tests here
+
+class ViewContentsTest(TestCase):
+  def test_homepage_contents(self):
+    response = self.client.get(reverse('homepage'))
+    self.assertContains(response, "<h2>Welcome to SkillPilot</h2>")
+    self.assertNotContains(response, "This string should not be in the homepage!")
+
+  # more tests here
