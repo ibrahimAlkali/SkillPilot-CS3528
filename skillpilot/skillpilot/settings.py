b4124feb5d4a5cace5118a6ca3b56353a54754e8
Mattia Di Profio <mattia.diprofio@gmail.com>
2024-02-08 13:24:30 +0000

    fix static files warning when running project

--- skillpilot/skillpilot/settings.py
+++ skillpilot/skillpilot/settings.py
@@ -3,7 +3,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -24,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core_app',
-
 ]
 
 MIDDLEWARE = [
